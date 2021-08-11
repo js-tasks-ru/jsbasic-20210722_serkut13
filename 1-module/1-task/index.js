9efbb264ad6d523d15610afc19e301086a490b84
serkut13 <serkut13@gmail.com>
2021-08-11 22:27:49 +0300

    [task-1]: добавил исключение для случая, когда факториал ==0

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,6 +1,8 @@
 function factorial(n) {
+    if (n == 0 || n == 1)
+    return 1
+  else
     for(var i = n - 1; i > 0; i--){
-         n *= i;
-     }
-     return n;
-   }
+      n *= i;
+  }
+  return n   }
