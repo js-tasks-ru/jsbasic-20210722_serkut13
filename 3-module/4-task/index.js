9f0f9bb788b24d08260381db84b17c45c11d321e
serkut13 <serkut13@gmail.com>
2021-08-16 03:55:59 +0300

    [3-module-4-task]: решение задачи с showSalary

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
 function showSalary(users, age) {
-  // ваш код...
+
+  let filtredUsers = users.filter(function(users){return users.age <= age }) 
+
+  return filtredUsers.map(users =>`${users.name}, ${users.balance}`).join('\n')
 }
