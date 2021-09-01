7023565077a31cc3d2c4be510a12bb2c1e3c6787
serkut13 <serkut13@gmail.com>
2021-09-01 14:58:37 +0300

    Решение карусели

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,31 @@
 function initCarousel() {
-  // ваш код...
+  let picture =  document.querySelector('.carousel__inner');
+  let ar =  document.querySelector('.carousel__arrow_right');
+  let al = document.querySelector('.carousel__arrow_left');
+let x = 0
+function hide(){
+switch (true) {
+  case x == 0: 
+  al.style.display = 'none';
+  break;
+  case x == 3:
+  ar.style.display = 'none';
+  break;
+  default:
+    al.style.display = 'block',   ar.style.display = 'block';
+    break;
+}}
+
+hide();
+  forward = () => { ++x, picture.style.transform = `translateX(-${x * picture.offsetWidth}px)`
+ hide()
+return x
+}
+backward = () => { --x, picture.style.transform = `translateX(${-x * picture.offsetWidth}px)`
+ hide()
+return x
+}
+
+  ar.addEventListener('click', forward)
+  al.addEventListener('click', backward)
 }
